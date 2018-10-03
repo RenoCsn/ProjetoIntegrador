4b4db7f18d1fd99d6a34aaf9d649270a65238ff9
Ledenir <39539745+Ledenir@users.noreply.github.com>
2018-10-03 14:47:29 -0300

    Classe Cnh

---
+++ src/classes/Cnh.java
@@ -0,0 +1,25 @@
+package classes;
+
+public class Cnh {
+  private String numero;  
+  private CategoriaCnh tipo_cnh;
+
+  public Cnh() {}
+
+  public String getNumero() {
+    return numero;
+  }
+
+  public void setNumero(String numero) {
+    this.numero = numero;
+  }
+
+  public CategoriaCnh getClasse() {
+    return tipo_cnh;
+  }
+
+  public void setClasse(CategoriaCnh classe) {
+    this.tipo_cnh = tipo_cnh;
+  }
+
+}
